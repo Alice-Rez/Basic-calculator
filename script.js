aa34e0ddce7dc0f8d1249e46ae56637bd1df7ca9
Alice-Rez <reznickova.alice@seznam.cz>
2020-10-10 21:22:01 +0200

    polishing

--- script.js
+++ script.js
@@ -10,11 +10,9 @@ function number(input) {
       resultFunction = 0;
     }
     number1 += input;
-    console.log("input:", input, "whole number 1", number1);
     document.getElementById("display").innerHTML = number1;
   } else {
     number2 += input;
-    console.log("input:", input, "whole number 2", number2);
     document.getElementById("display").innerHTML = number2;
   }
 }
@@ -31,19 +29,9 @@ function decimal() {
 
 function markFunction() {
   if (operation === "") {
-    // if (number1.charAt(0) !== "-") {
-    //   number1 = "-" + number1;
-    // } else {
-    //   number1 = number1.substr(1, number1.length - 1);
-    // }
     number1 = `${number1 * -1}`;
     document.getElementById("display").innerHTML = number1;
   } else {
-    // if (number2.charAt(0) !== "-") {
-    //   number2 = "-" + number2;
-    // } else {
-    //   number2 = number2.substr(1, number2.length - 1);
-    // }
     number2 = `${number2 * -1}`;
     document.getElementById("display").innerHTML = number2;
   }
@@ -53,14 +41,6 @@ function operationFunction(input) {
   if (number2 !== "") {
     number1 = parseFloat(number1);
     number2 = parseFloat(number2);
-    console.log(
-      "number1:",
-      number1,
-      "number2:",
-      number2,
-      "operation:",
-      operation
-    );
     switch (operation) {
       case "+":
         var resultBetween = number1 + number2;
@@ -83,26 +63,16 @@ function operationFunction(input) {
     }
     document.getElementById("display").innerHTML = resultBetween;
     document.getElementById(`${operation}`).style.backgroundColor = "";
-    console.log(resultBetween);
     number1 = `${resultBetween}`;
     number2 = "";
   }
   operation = input;
-  console.log("current operation is:", operation);
   document.getElementById(`${input}`).style.backgroundColor = "darkgray";
 }
 
 function calculator() {
   number1 = parseFloat(number1);
   number2 = parseFloat(number2);
-  console.log(
-    "number1:",
-    number1,
-    "number2:",
-    number2,
-    "operation:",
-    operation
-  );
   switch (operation) {
     case "+":
       resultFunction = number1 + number2;
@@ -123,9 +93,11 @@ function calculator() {
       resultFunction = "Error ";
       break;
   }
+  if (resultFunction.toString().length >= 12) {
+    resultFunction = resultFunction.toExponential(5);
+  }
   document.getElementById("display").innerHTML = resultFunction;
   document.getElementById(`${operation}`).style.backgroundColor = "";
-  console.log(resultFunction);
   number1 = `${resultFunction}`;
   number2 = "";
   operation = "";
